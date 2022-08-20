5d9b8ac3b6e7eba2cd56e75c9091c42da21818e6
miguelleonmarti <miguelleonmarti@gmail.com>
2022-08-20 22:17:46 +0100

    feature: MyToken smart contract

---
+++ hardhat/contracts/MyToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(
+        uint256 initialSupply,
+        string memory tokenName,
+        string memory tokenSymbol
+    ) ERC20(tokenName, tokenSymbol) {
+        _mint(msg.sender, initialSupply);
+    }
+}
