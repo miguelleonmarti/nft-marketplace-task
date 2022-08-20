3371baf5ed223435c74c18e78c4932f1bb765234
miguelleonmarti <miguelleonmarti@gmail.com>
2022-08-20 23:13:43 +0100

    feature: added rainbowkit (connect button)

---
+++ frontend/config.ts
@@ -0,0 +1,3 @@
+export const app: { [key: string]: string } = {
+  name: "NFT Marketplace",
+};
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import { ReactNode, useState, useEffect } from "react";
+import "@rainbow-me/rainbowkit/styles.css";
+import { darkTheme, getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { app } from "../config";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const { chains, provider } = configureChains([chain.polygon], [alchemyProvider({ apiKey: process.env.ALCHEMY_API_KEY }), publicProvider()]);
+
+const { connectors } = getDefaultWallets({ appName: app.name, chains });
+
+const wagmiClient = createClient({ autoConnect: true, connectors, provider });
+
+function Web3Wrapper({ children }: { children: ReactNode }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains} showRecentTransactions theme={darkTheme()}>
+        {children}
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Web3Wrapper>
+      <Component {...pageProps} />
+    </Web3Wrapper>
+  );
+}
+
+export default MyApp;
