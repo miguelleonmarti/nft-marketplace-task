9cd73ae50f408cf7db639eb3714417ef7a1226bc
miguelleonmarti <miguelleonmarti@gmail.com>
2022-08-21 12:17:11 +0100

    feature: home page or listings (without functionaly)

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,35 @@
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
+import styles from "../styles/Home.module.scss";
+import { Table, Button } from "@web3uikit/core";
+
+const header: string[] = ["Address", "ID", "Price", "Owner", "Button"];
+
+const BuyButton = () => {
+  return <Button onClick={() => console.log("mint")} theme="secondary" size="large" text="Buy"></Button>;
+};
+
+const data = [
+  ["0x123456789", "130", "18.0", "0x0000000", <BuyButton key={"130"} />],
+  ["0x123456789", "65", "27.0", "0x0000000", <BuyButton key={"65"} />],
+  ["0x123456789", "4", "48.0", "0x0000000", <BuyButton key={"4"} />],
+  ["0x123456789", "1", "199.0", "0x0000000", <BuyButton key={"1"} />],
+];
 
 const Home: NextPage = () => {
-  return <div>It works!</div>;
+  return (
+    <section className={styles.main}>
+      <Table
+        columnsConfig="4fr 2fr 2fr 4fr 2fr"
+        data={data}
+        header={header}
+        isColumnSortable={[false, true, true, false]}
+        maxPages={3}
+        onPageNumberChanged={function noRefCheck() {}}
+        onRowClick={function noRefCheck() {}}
+        pageSize={2}
+      ></Table>
+    </section>
+  );
 };
 
 export default Home;
