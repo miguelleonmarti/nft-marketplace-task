48c5630f2e538aa8a97ee6b2f918014bb2413ae8
miguelleonmarti <miguelleonmarti@gmail.com>
2022-08-21 16:02:41 +0100

    chore: contracts deployed on the goerli testnet

--- hardhat/contracts/MyToken.sol
+++ hardhat/contracts/MyToken.sol
@@ -11,4 +11,8 @@ contract MyToken is ERC20 {
     ) ERC20(tokenName, tokenSymbol) {
         _mint(msg.sender, initialSupply);
     }
+
+    function mint(uint256 amount) public {
+        _mint(msg.sender, amount);
+    }
 }
--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,9 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
+import "dotenv/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+console.log(ETHERSCAN_API_KEY);
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
