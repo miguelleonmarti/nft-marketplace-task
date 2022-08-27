24f68568298fa4c349d35b03ae933468cb3a2078
miguelleonmarti <miguelleonmarti@gmail.com>
2022-08-27 18:36:30 +0100

    feature: home page (fill orders)

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,28 +1,108 @@
 import type { NextPage } from "next";
+import { useState, useEffect, useContext } from "react";
 import styles from "../styles/Home.module.scss";
-import { Table, Button } from "@web3uikit/core";
+import { Table, Button, useNotification } from "@web3uikit/core";
+import { Web3Context } from "../contexts/Web3Context";
+import { useAccount } from "wagmi";
+import { SwappableAssetV4 } from "@traderxyz/nft-swap-sdk";
 
-const header: string[] = ["Address", "ID", "Price", "Owner", "Button"];
-
-const BuyButton = () => {
-  return <Button onClick={() => console.log("mint")} theme="secondary" size="large" text="Buy"></Button>;
+const BuyButton = ({ handleOnClick, signedOrder }) => {
+  return <Button onClick={() => handleOnClick(signedOrder)} theme="secondary" size="large" text="Buy"></Button>;
 };
 
-const data = [
-  ["0x123456789", "130", "18.0", "0x0000000", <BuyButton key={"130"} />],
-  ["0x123456789", "65", "27.0", "0x0000000", <BuyButton key={"65"} />],
-  ["0x123456789", "4", "48.0", "0x0000000", <BuyButton key={"4"} />],
-  ["0x123456789", "1", "199.0", "0x0000000", <BuyButton key={"1"} />],
-];
-
 const Home: NextPage = () => {
+  const [orders, setOrders] = useState([]);
+  const { swapSdk } = useContext(Web3Context);
+  const { address } = useAccount();
+  const dispatch = useNotification();
+
+  function truncateEthAddress(address: string) {
+    const truncateRegex = /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/;
+    const match = address.match(truncateRegex);
+    if (!match) return address;
+    return `${match[1]}…${match[2]}`;
+  }
+
+  async function handleBuy(signedOrder: any) {
+    try {
+      const token: SwappableAssetV4 = { tokenAddress: signedOrder.erc20Token, type: "ERC20", amount: signedOrder.erc20TokenAmount };
+      const approvalStatus = await swapSdk.loadApprovalStatus(token, address);
+
+      if (!approvalStatus.contractApproved) {
+        const approvalTx = await swapSdk.approveTokenOrNftByAsset(token, address);
+        const approvalTxReceipt = await approvalTx.wait();
+        console.log(`Approved ${token.tokenAddress} contract to swap with 0x. TxHash: ${approvalTxReceipt.transactionHash})`);
+      }
+
+      delete signedOrder.createdAt;
+      delete signedOrder.updatedAt;
+      delete signedOrder.id;
+      delete signedOrder.verifyingContract;
+
+      const fillTx = await swapSdk.fillSignedOrder(
+        {
+          ...signedOrder,
+          expiry: String(signedOrder.expiry),
+          erc721TokenId: String(signedOrder.erc721TokenId),
+          erc20TokenAmount: String(signedOrder.erc20TokenAmount),
+          nonce: BigInt(signedOrder.nonce),
+        },
+        {},
+        { gasLimit: BigInt(300000) }
+      );
+      const fillTxReceipt = await swapSdk.awaitTransactionHash(fillTx.hash);
+      console.log({ fillTxReceipt });
+      console.log(`🎉 🥳 Order filled. TxHash: ${fillTxReceipt.transactionHash}`);
+      dispatch({
+        type: "success",
+        message: "Item bought!",
+        title: "New Notification",
+        position: "topL",
+      });
+    } catch (error) {
+      console.log({ error });
+      dispatch({
+        type: "error",
+        message: error.reason ?? error.message,
+        title: "New Notification",
+        position: "topL",
+      });
+    }
+  }
+
+  function formatOrderData() {
+    if (!address) return;
+    return orders
+      .filter(({ maker }) => address.toLowerCase() !== maker)
+      .map((order) => [
+        truncateEthAddress(order.erc721Token),
+        order.erc721TokenId,
+        truncateEthAddress(order.erc20Token),
+        order.erc20TokenAmount,
+        <BuyButton key={order.nonce} handleOnClick={handleBuy} signedOrder={order} />,
+      ]);
+  }
+
+  useEffect(() => {
+    if (!address) return;
+
+    async function getOrders() {
+      const response = await fetch("/api/orders");
+      const orders = await response.json();
+      setOrders(orders);
+    }
+
+    getOrders();
+  }, [address]);
+
   return (
     <section className={styles.main}>
       <Table
-        columnsConfig="4fr 2fr 2fr 4fr 2fr"
-        data={data}
-        header={header}
-        isColumnSortable={[false, true, true, false]}
+        columnsConfig="2fr 2fr 2fr 2fr 2fr"
+        isLoading={!orders}
+        data={formatOrderData()}
+        header={["NFT Address", "NFT ID", "Token Address", "Token Amount", "Actions"]}
+        isColumnSortable={[true, true, true, true, false]}
         maxPages={3}
         onPageNumberChanged={function noRefCheck() {}}
         onRowClick={function noRefCheck() {}}
