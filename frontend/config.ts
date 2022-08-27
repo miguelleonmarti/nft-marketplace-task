82abbe1df68dd397784b22b6bf1918cc36f92d5f
miguelleonmarti <miguelleonmarti@gmail.com>
2022-08-27 18:26:56 +0100

    chore: app config

--- frontend/config.ts
+++ frontend/config.ts
@@ -2,4 +2,5 @@ export const app: { [key: string]: string } = {
   name: "NFT Marketplace",
   tokenAddress: "0xa0a021355Dc586752A4C9Cd9Ece0F699912e7EB4",
   nftAddress: "0x52F50B4614FD548C2Adede7689712F9aa0Ca603c",
+  zeroExExchangeProxyContractAddress: "0xdef1c0ded9bec7f1a1670819833240f027b25eff",
 };
