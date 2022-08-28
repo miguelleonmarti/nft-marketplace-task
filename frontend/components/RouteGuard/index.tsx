8958781616ae81c2e5c247304c3fad995fb7a190
miguelleonmarti <miguelleonmarti@gmail.com>
2022-08-28 11:33:33 +0100

    fix: routeGuard component error
    Forgot to check if user is connected or not

--- frontend/components/RouteGuard/index.tsx
+++ frontend/components/RouteGuard/index.tsx
@@ -5,18 +5,18 @@ import useNotifications from "@/hooks/notifications";
 
 export default function RouteGuard({ protectedRoutes, children }) {
   const router = useRouter();
-  const { address } = useAccount();
+  const { isConnected } = useAccount();
   const { notifyNoWalletConnection } = useNotifications();
 
   const pathIsProtected = protectedRoutes.indexOf(router.pathname) !== -1;
 
   useEffect(() => {
-    if (pathIsProtected) {
+    if (pathIsProtected && !isConnected) {
       router.push("/");
       notifyNoWalletConnection();
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [address, router]);
+  }, [isConnected, router]);
 
   return children;
 }
