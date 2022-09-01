ebc27f5a9a4103c640cb7ceaf7482d0975be761a
miguelleonmarti <miguelleonmarti@gmail.com>
2022-09-01 18:59:07 +0100

    fix: hardhat private key error fixed

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -4,14 +4,21 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-etherscan";
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
-console.log(ETHERSCAN_API_KEY);
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  defaultNetwork: "hardhat",
   networks: {
+    // hardhat: {
+    //   forking: {
+    //     url: `https://ropsten.infura.io/v3/${INFURA_API_KEY}`,
+    //     blockNumber: 12901110,
+    //     enabled: true,
+    //   },
+    // },
     goerli: {
       url: `https://goerli.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [PRIVATE_KEY],
