1abfba95e46f8f8533a37fe50ffaaf596ef3e649
miguelleonmarti <miguelleonmarti@gmail.com>
2022-09-02 21:20:20 +0100

    fix: multiples orders for same nft avoided

--- frontend/pages/api/order/index.ts
+++ frontend/pages/api/order/index.ts
@@ -7,6 +7,8 @@ export default async function handle(req: NextApiRequest, res: NextApiResponse) 
   if (req.headers.authorization !== process.env.NEXT_PUBLIC_API_TOKEN) return res.status(401).send("Unauthorized");
   const order: Order = JSON.parse(req.body);
   if (req.method !== "POST") return;
+  const alreadyCreated = await prisma.order.count({ where: { erc721TokenId: order.erc721TokenId } });
+  if (alreadyCreated) return res.status(409).send("Already exists");
   const result = await prisma.order.create({
     data: { ...order },
   });
